400a98d46d6a71e9ebda7f8db905cf1ac5e3bf01
e.davydov <davidoveg@gmail.com>
2024-09-18 11:27:04 +0700

    хочу сделать java проект

---
+++ lab1/sql/insert/insert.sql
@@ -0,0 +1,8 @@
+insert into person (name, second_name, phone) VALUES ('peopleName1', 'peopleSecondName1', '77777777701');
+insert into person (name, second_name, phone) VALUES ('peopleName2', 'peopleSecondName2', '77777777702');
+insert into person (name, second_name, phone) VALUES ('peopleName3', 'peopleSecondName3', '77777777703');
+
+insert into staff values (1, 70000, 'doctor');
+insert into staff values (2, 150000, 'doctor');
+insert into staff values (3, 30000, 'cleaner');
+

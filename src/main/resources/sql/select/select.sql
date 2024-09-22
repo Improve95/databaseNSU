d061e76efdccd109af4bd4a4192fdf44c5f9229d
e.davydov <davidoveg@gmail.com>
2024-09-22 16:33:21 +0700

    сделал третий запрос

--- src/main/resources/sql/select/select.sql
+++ src/main/resources/sql/select/select.sql
@@ -6,36 +6,36 @@ select distinct patient.* from patient inner join disease d on patient.id = d.pa
     where d.name = 'unknown_disease' and
           patient.coming_time > current_date - interval '3 months';
 
+select sum(patient_number), doctor_id from (
+    select count(*) as patient_number,
+           case
+               when p.status = 'HEALTHY' then 'HEALTHY'
+               else 'OTHER'
+               end as status_group, doctor_id from doctor inner join patient p on doctor.staff_id = p.doctor_id
+    group by (status_group, p.doctor_id)
+)
+group by doctor_id
+order by doctor_id desc ;
 
+select count(*) as patient_number,
+       case
+           when p.status = 'HEALTHY' then 'HEALTHY'
+           else 'OTHER'
+           end as status_group, doctor_id from doctor inner join patient p on doctor.staff_id = p.doctor_id
+group by (status_group, p.doctor_id);
 
-select doctor.*, count(*) from doctor inner join patient p on doctor.staff_id = p.doctor_id
-    where p.status = 'HEALTHY'
-    group by (staff_id)
-    order by count(*) desc;
-
-select doctor.*, count(*) from doctor inner join patient p on doctor.staff_id = p.doctor_id
-    group by (staff_id)
-    order by count(*) desc;
---     fetch next 10 rows only;
-
-select count(*) / count(*) over (partition by p.status) as "% of total"
-    from doctor inner join patient p on doctor.staff_id = p.doctor_id
-        where p.status = 'HEALTHY';
-
-select count(*) from doctor inner join patient p on doctor.staff_id = p.doctor_id
-        where p.status = 'HEALTHY'
-    union select count(*) from doctor inner join patient p on doctor.staff_id = p.doctor_id
-        where p.status = 'OUT_TREATMENT' or p.status = 'TREATMENT_IN_ANOTHER_PLACE';
-
-select count(*), p.status, doctor_id from doctor inner join patient p on doctor.staff_id = p.doctor_id
-    group by (p.status='HEALTHY', p.doctor_id)
-    order by doctor_id desc;
-
-select count(*) from doctor inner join patient p on doctor.staff_id = p.doctor_id
-    group by (p.status='HEALTHY')
-    order by count(*) desc;
-
-select total, doctor_id from (
-select count(*) as total, doctor_id from doctor inner join patient p on doctor.staff_id = p.doctor_id
-group by (p.status='HEALTHY', p.doctor_id)
-order by doctor_id desc);
+select *, healthy_patient / total_patient * 100 as "%" from (
+    select doctor_id,
+           sum(case when status_group = 'HEALTHY' then patient_number else 0 end) as healthy_patient,
+           sum(patient_number) as total_patient from (
+           select * from (
+                select count(*) as patient_number,
+                case
+                    when p.status = 'HEALTHY' then 'HEALTHY'
+                    else 'OTHER'
+                    end as status_group, doctor_id from doctor inner join patient p on doctor.staff_id = p.doctor_id
+                                                   group by (status_group, p.doctor_id)
+            )
+    ) group by doctor_id)
+order by "%" desc
+fetch next 10 rows only;

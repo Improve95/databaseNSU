9e296aa1f55806d79c0a16fb4a6b899668f12c84
e.davydov <davidoveg@gmail.com>
2024-09-22 16:50:07 +0700

    переделал запрос, теперь он поменьше

--- src/main/resources/sql/select/select.sql
+++ src/main/resources/sql/select/select.sql
@@ -6,36 +6,8 @@ select distinct patient.* from patient inner join disease d on patient.id = d.pa
     where d.name = 'unknown_disease' and
           patient.coming_time > current_date - interval '3 months';
 
-select sum(patient_number), doctor_id from (
-    select count(*) as patient_number,
-           case
-               when p.status = 'HEALTHY' then 'HEALTHY'
-               else 'OTHER'
-               end as status_group, doctor_id from doctor inner join patient p on doctor.staff_id = p.doctor_id
-    group by (status_group, p.doctor_id)
-)
-group by doctor_id
-order by doctor_id desc ;
-
-select count(*) as patient_number,
-       case
-           when p.status = 'HEALTHY' then 'HEALTHY'
-           else 'OTHER'
-           end as status_group, doctor_id from doctor inner join patient p on doctor.staff_id = p.doctor_id
-group by (status_group, p.doctor_id);
-
-select *, healthy_patient / total_patient * 100 as "%" from (
-    select doctor_id,
-           sum(case when status_group = 'HEALTHY' then patient_number else 0 end) as healthy_patient,
-           sum(patient_number) as total_patient from (
-           select * from (
-                select count(*) as patient_number,
-                case
-                    when p.status = 'HEALTHY' then 'HEALTHY'
-                    else 'OTHER'
-                    end as status_group, doctor_id from doctor inner join patient p on doctor.staff_id = p.doctor_id
-                                                   group by (status_group, p.doctor_id)
-            )
-    ) group by doctor_id)
-order by "%" desc
-fetch next 10 rows only;
+select *, healthy_patient::NUMERIC / patient_number * 100 as "%" from (
+    select count(*) as patient_number, sum(case when p.status = 'HEALTHY' then 1 else 0 end) as healthy_patient,
+           doctor_id from doctor inner join patient p on doctor.staff_id = p.doctor_id
+    group by (p.doctor_id)
+);

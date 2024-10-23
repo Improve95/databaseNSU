e612482a01df84935bff8ca34b07808b367424cd
e.davydov <e.davydov@g.nsu.ru>
2024-10-24 00:16:48 +0700

    commit

--- zoo/sql/select.sql
+++ zoo/sql/select.sql
@@ -1,8 +1,1 @@
-select * from animal a where a.animal_type = (
-    select animal_type from animal
-    group by animal_type
-    order by count(*)
-    fetch first 1 rows only);
-
-
-select * from
+select *, count(*) over (partition by (select habitat from animal_type at where at.id = a.animal_type)) from animal a;

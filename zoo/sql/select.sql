1ceaee585d1a571dbec21f7c8fd6fcc8ca0241e2
e.davydov <e.davydov@g.nsu.ru>
2024-10-24 16:22:17 +0700

    добавил 3 и 4 запрос

--- zoo/sql/select.sql
+++ zoo/sql/select.sql
@@ -1,3 +1,4 @@
+/* == 1 == */
 select * from (
     select *, max(ah.animal_in_habitat) over() as max_habitat from (
         select a.*,
@@ -6,14 +7,36 @@ select * from (
         from animal a) as ah)
 where animal_in_habitat = max_habitat;
 
+/* == 2 == */
 select habitat, min(min_coming_time) from (
     select *, min(animal_habitat_table.coming_time) over (partition by animal_habitat_table.habitat) as min_coming_time from (
         select a.*, (select habitat from animal_type at
                     where a.animal_type = at.id) as habitat from animal a) as animal_habitat_table)
 group by habitat;
 
+/* == 3 == */
 select cage.*, light_weight_animal from cage inner join (
     select cage, min(weight) light_weight_animal from animal
     group by cage
     ) as light_weight_cage on cage.id = light_weight_cage.cage;
 
+/* == 4 == */
+select * from (
+    select *, max(max_in_cage) over () as max_in_cage_between_all_cages from (
+    select cage, total_weight, max(animals_in_cage) as max_in_cage from (
+        select a.*,
+        sum(a.weight) over (partition by cage) as total_weight,
+        count(*) over (partition by cage) as animals_in_cage
+        from animal a)
+    group by cage, total_weight
+    order by max_in_cage desc))
+where max_in_cage = max_in_cage_between_all_cages;
+
+select * from (
+    select id as animal_id, weight, animals_in_cage, max(animals_in_cage) over () as max_in_cage_between_all_cages from (
+        select a.*,
+        count(*) over (partition by cage) as animals_in_cage
+        from animal a)) as cmp_animals_in_cage
+where animals_in_cage = max_in_cage_between_all_cages;
+
+/* == 5 == */

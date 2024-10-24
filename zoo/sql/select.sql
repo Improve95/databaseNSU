2a6308d0f4a82326b551280ff72fe7e925032207
e.davydov <e.davydov@g.nsu.ru>
2024-10-24 12:01:47 +0700

    сделал первый запрос

--- zoo/sql/select.sql
+++ zoo/sql/select.sql
@@ -1,1 +1,9 @@
-select *, count(*) over (partition by (select habitat from animal_type at where at.id = a.animal_type)) from animal a;
+select * from (
+    select *, max(ah.animal_in_habitat) over() as max_habitat from (
+        select a.*,
+        count(*) over (partition by (select habitat from animal_type at
+                                     where at.id = a.animal_type)) as animal_in_habitat
+        from animal a) as ah)
+where animal_in_habitat = max_habitat;
+
+

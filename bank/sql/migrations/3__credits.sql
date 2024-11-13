593c228d3729a86a1483228cc665d1db5cf1e893
e.davydov <e.davydov@g.nsu.ru>
2024-11-13 17:30:02 +0700

    значительно добавил данных в таблицы

--- bank/sql/migrations/3__credits.sql
+++ bank/sql/migrations/3__credits.sql
@@ -1,27 +1,43 @@
+create type credit_status as enum ('open', 'close');
+drop table if exists credits cascade ;
 create table credits (
     id uuid primary key default gen_random_uuid() ,
-    remaining_debt int not null check ( remaining_debt > 0 ),
-    current_percent int not null check ( current_percent > 0 ),
-    /* комиссии? commission_amount int not null check ( commission_amount >= 0 ) ,*/
-    /* штрафы ?? */
-    client_id int not null references clients("id") on delete set null
+    initial_debt float4 not null check ( initial_debt >= 0 ) ,
+    remaining_debt float4 not null check ( remaining_debt > 0 ),
+    period interval not null ,
+    percent int not null check ( percent > 0 ),
+    client_id int not null references clients("id") on delete set null,
+    credit_status credit_status not null default 'open'::credit_status
 );
 
+create table commissions (
+    credit_id uuid not null references credits("id") ,
+    amount float4 not null check ( amount > 0 ) ,
+    date date not null default current_date ,
+    primary key (credit_id, date)
+);
+
+create table penalties (
+    credit_id uuid not null references credits("id") ,
+    amount float4 not null check ( amount > 0 ) ,
+    date date not null default current_date ,
+    primary key (credit_id, date)
+);
+
+drop table if exists payments_schedule;
 create table payments_schedule (
     credit_id uuid not null references credits("id") on delete cascade ,
-    debt_less_then int not null check ( debt_less_then >= 0 ) ,
+    month_amount float4 not null check ( month_amount >= 0 ) ,
     payment_date timestamp with time zone not null,
     primary key (credit_id, payment_date)
 );
 
 create type payment_type as enum ('bank_account', 'qiwi', 'yandex_pay');
+drop table if exists payments;
 create table payments (
     id uuid default gen_random_uuid() ,
     amount int not null check ( amount > 0 ) ,
     type_of_payment payment_type not null ,
     date timestamp with time zone not null default current_timestamp ,
-    client_id int not null references clients("id") on delete cascade ,
     credit_id uuid not null references credits("id") on delete set null
 );
-
-alter table payments drop column client_id;

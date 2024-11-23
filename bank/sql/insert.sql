cc0645d63838a01197f3bf683e6eaacbb93fa796
e.davydov <e.davydov@g.nsu.ru>
2024-11-23 20:46:03 +0700

    начинаю добавлять скрипты

---
+++ bank/sql/insert.sql
@@ -0,0 +1,4 @@
+insert into positions (name) values ('director'),
+                                    ('manager1'),
+                                    ('manager2'),
+                                    ('employee');

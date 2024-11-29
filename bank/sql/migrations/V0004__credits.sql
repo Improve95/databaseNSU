345a4bffab86b9b1e0d7c31b198211dfa91e59e5
e.davydov <e.davydov@g.nsu.ru>
2024-11-29 21:27:29 +0700

    почему не работает сравнение при группировке

--- bank/sql/migrations/V0004__credits.sql
+++ bank/sql/migrations/V0004__credits.sql
@@ -1,12 +1,12 @@
 create type credit_status as enum ('open', 'close');
 create table credits (
     id uuid primary key default gen_random_uuid() ,
-    initial_debt float4 not null check ( initial_debt >= 0 ) ,
-    remaining_debt float4 not null check ( remaining_debt >= 0 ) ,
+    initial_debt numeric(15, 2) not null check ( initial_debt >= 0 ) ,
+    remaining_debt numeric(15, 2) not null check ( remaining_debt >= 0 ) ,
     taking_date date not null default current_date,
     percent int not null check ( percent > 0 ),
     credit_period interval not null ,
-    month_amount float4 not null check ( month_amount > 0 ) ,
+    month_amount numeric(15, 2) not null check ( month_amount > 0 ) ,
     credit_tariff_id int not null references credit_tariffs("id") on delete set null ,
     client_id int not null references clients("id") on delete set null,
     credit_status credit_status not null default 'open'::credit_status
@@ -15,7 +15,7 @@ create table credits (
 create table penalties (
     id uuid primary key default gen_random_uuid() ,
     credit_id uuid not null references credits("id") ,
-    amount float4 not null check ( amount > 0 ) ,
+    amount numeric(15, 2) not null check ( amount > 0 ) ,
     penalty_date date not null default current_date ,
     paid_deadline date not null default current_date + interval '1 month' ,
     is_paid boolean not null default false
@@ -28,7 +28,7 @@ create table payments_schedule (
     primary key (credit_id, up_to_payment_date)
 );
 
-create type payment_type as enum ('mountly', 'main_debt', 'penalty');
+create type payment_type as enum ('month', 'main_debt');
 create type payment_way as enum ('bank_account', 'qiwi', 'yandex_pay');
 create table payments (
     id uuid default gen_random_uuid() ,
@@ -42,7 +42,7 @@ create table payments (
 create table balances (
     id uuid primary key default gen_random_uuid() ,
     credit_id uuid not null references credits("id") ,
---     remaining_debt float4 not null check ( remaining_debt > 0 ) ,
-    accrued_by_percent float4 not null check ( accrued_by_percent > 0 ) ,
+    remaining_debt numeric(15, 2) not null check ( remaining_debt > 0 ) ,
+    accrued_by_percent numeric(15, 2) not null check ( accrued_by_percent > 0 ) ,
     date date not null default current_date
 );
--- bank/sql/select.sql
+++ bank/sql/select.sql
@@ -17,18 +17,10 @@ select count(*), sum(initial_debt), avg(initial_debt),
        (select c2.client_id from credits c2 where c2.credit_tariff_id = c1.credit_tariff_id and
         c2.initial_debt = (select max(c3.initial_debt) from credits c3 where c3.credit_tariff_id = c1.credit_tariff_id)) as client_with_max_debt
 from credits c1
-where taking_date between current_timestamp - interval '2 month' and current_timestamp - interval '20 days'
+where taking_date between current_timestamp - interval '3 month' and current_timestamp - interval '20 days'
 group by credit_tariff_id) inner join clients c on c.id = client_with_max_debt;
 
 /* == 2 == */
-select DATE_TRUNC('year', p.date) AS year,
-       DATE_TRUNC('month', p.date) AS month,
-       DATE_TRUNC('day', p.date) AS day,
-       count(*), sum(amount) from payments p
-where date between current_timestamp - interval '2 month' and current_timestamp - interval '20 days'
-group by rollup (year, month, day)
-order by day, month, year;
-
 select DATE_TRUNC('year', p.date) AS year,
        DATE_TRUNC('month', p.date) AS month,
        DATE_TRUNC('day', p.date) AS day,
@@ -48,6 +40,15 @@ order by tmp.depth;
 
 /* == 4 == */
 -- pnp_number - paid_not_paid_number
+select count(*) from payments_schedule where is_paid = false;
+select count(*) from payments_schedule where credit_id = '00003ed6-c719-464b-913c-df308904c50b';
+select sum(case when ps.is_paid = true then 1 else 0 end) as paid_number,
+       sum(case when ps.is_paid = false then 1 else 0 end) as not_paid_number,
+       credit_id
+from payments_schedule ps
+group by credit_id
+order by credit_id;
+
 select c.id, paid_number, not_paid_number,
        (paid_number + not_paid_number) / not_paid_number * 100.0 as np_percent,
        not_paid_number * c.month_amount as not_paid_sum
@@ -68,11 +69,12 @@ select sum_pay_credit.*, payment_sum / give_credit_sum as profit from (
 ) as sum_pay_credit;
 
 /* == 6 == */
+select * from credits where remaining_debt = 0;
 select pay_sum / initial_debt as profit, credit_id from (
-    select c.id as credit_id, pay_sum, c.initial_debt from credits c right join (
+    select c.id as credit_id, pay_sum, c.initial_debt, c.id from credits c right join (
         select sum(p.amount) as pay_sum, p.credit_id from payments p
         group by p.credit_id
     ) as paymens on c.id = paymens.credit_id
-    where c.taking_date between current_timestamp - interval '2 month' and current_timestamp - interval '3 days' and
+    where c.taking_date >= current_timestamp - interval '5 month' and
           c.taking_date + c.credit_period <= current_date - interval '2 month' and
-          c.credit_status = 'close'::credit_status);
+          c.credit_status = 'close');

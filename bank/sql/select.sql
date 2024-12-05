00bbe987e5ae6078f0cadffc74d9b2df842a3138
e.davydov <e.davydov@g.nsu.ru>
2024-12-05 14:58:32 +0700

    исправляю запросы

--- bank/sql/select.sql
+++ bank/sql/select.sql
@@ -42,44 +42,37 @@ order by tmp.depth;
 /* == 4 == */
 -- pnp_number - paid_not_paid_number
 select c.id, paid_number, not_paid_number,
-       (paid_number + not_paid_number) / 100.0 * not_paid_number as percent,
-       not_paid_sum
+       not_paid_number::float / (paid_number + not_paid_number) * 100.0 as not_paid_count_percent,
+       paid_sum, not_paid_sum,
+       not_paid_sum::float / (paid_sum + not_paid_sum) * 100.0 as not_paid_sum_percent
 from credits c inner join (
     select sum(case when ps.is_paid = true then 1 else 0 end) as paid_number,
            sum(case when ps.is_paid = false then 1 else 0 end) as not_paid_number,
+           sum(case when ps.is_paid = true then ps.amount else 0 end) as paid_sum,
            sum(case when ps.is_paid = false then ps.amount else 0 end) as not_paid_sum,
            credit_id
     from payments_schedule ps
+    where ps.deadline <= current_timestamp
     group by credit_id
-) as pnp_number on c.id = pnp_number.credit_id;
+) as pnp_number on c.id = pnp_number.credit_id
+where not_paid_number > 0;
 
 /* == 5 == */
-select sum_pay_credit.*, payment_sum / give_credit_sum as profit from (
-    select sum(p.amount) as payment_sum,
-           sum(c.initial_debt) as give_credit_sum
+select sum_pay_credit.*, payment_sum / initial_debt as profit from (
+    select c.id as credit_id,
+            sum(p.amount) as payment_sum,
+           c.initial_debt
            from payments p inner join credits c on c.id = p.credit_id
-    where c.taking_date + c.credit_period between current_timestamp - interval '2 month' and current_timestamp + interval '2 year'
+    where c.taking_date + c.credit_period between current_timestamp - interval '1 year' and current_timestamp
+    group by c.id
 ) as sum_pay_credit;
 
 /* == 6 == */
-select pay_credit_sum.*, payment_sum / give_credit_sum as profit from (
-    select p.credit_id,
+select pay_credit_sum.*, payment_sum / initial_debt as profit from (
+    select c.id as credit_id,
            sum(p.amount) as payment_sum,
-           sum(c.initial_debt) as give_credit_sum
-    from payments_schedule p inner join credits c on p.credit_id = c.id
+           c.initial_debt
+    from payments p inner join credits c on p.credit_id = c.id
         where (c.taking_date between current_timestamp - interval '5 month' and current_timestamp) and
-              p.deadline <= current_timestamp + interval '2 month'
-    group by p.credit_id) as pay_credit_sum;
-
-
-/* ============================================================================================================ */
-
-
-/* == 1 == */
-select * from (
-select count(*), sum(initial_debt), avg(initial_debt),
-       (select c2.client_id from credits c2 where c2.credit_tariff_id = c1.credit_tariff_id and
-        c2.initial_debt = (select max(c3.initial_debt) from credits c3 where c3.credit_tariff_id = c1.credit_tariff_id)) as client_with_max_debt
-from credits c1
-where taking_date between current_timestamp - interval '3 month' and current_timestamp - interval '20 days'
-group by credit_tariff_id) inner join clients c on c.id = client_with_max_debt;
+              p.date <= current_timestamp + interval '2 month'
+    group by c.id) as pay_credit_sum;

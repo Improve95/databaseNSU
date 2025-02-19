a13df08b2a90319c2b116edc04eda79a1aba302b
e.davydov <e.davydov@g.nsu.ru>
2025-02-19 13:54:29 +0700

    дикстра работает

--- train/insertGen/graph.py
+++ train/insertGen/graph.py
@@ -3,8 +3,8 @@ import networkx as nx
 import random
 
 def main():
-    num_nodes = 30
-    num_edges = 250
+    num_nodes = 6
+    num_edges = 7
     seed = 123
     G = nx.gnm_random_graph(num_nodes, num_edges, seed)
 
@@ -32,18 +32,16 @@ def main():
             popIndex = routesList.pop(randomIndex)
             routes.add((i, popIndex))
         
-    print(routes)
+    # print(routes)
     print(routes.__len__())
 
-    
-
-    # pos = nx.spring_layout(G)
-    # plt.figure(figsize=(8, 6))
-    # nx.draw(G, pos, with_labels=True, node_color="lightblue", edge_color="gray", node_size=800, font_size=10)
-    # edge_labels = {(u, v): G[u][v]['weight'] for u, v in G.edges()}
-    # nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=10, font_color="red")
-    # plt.title(f"Случайный граф G({num_nodes}, {num_edges}) с весами рёбер")
-    # plt.show()
+    pos = nx.spring_layout(G)
+    plt.figure(figsize=(8, 6))
+    nx.draw(G, pos, with_labels=True, node_color="lightblue", edge_color="gray", node_size=800, font_size=10)
+    edge_labels = {(u, v): G[u][v]['weight'] for u, v in G.edges()}
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=10, font_color="red")
+    plt.title(f"Случайный граф G({num_nodes}, {num_edges}) с весами рёбер")
+    plt.show()
 
 if __name__ == "__main__":
     main()
--- train/insertGen/insert.py
+++ train/insertGen/insert.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import random
 
+INTEGER_MAX = 2147483647
+
 hostname = 'localhost'
 database = 'databaseNsu'
 username = 'postgres'
@@ -78,27 +80,55 @@ def insertPassengers(dbConnect):
         insertScript = "insert into passengers (name, passport_series, passport_number) values (%s, %s, %s)"
         cursor.executemany(insertScript, passengers)
 
-num_nodes = 30
-num_edges = 250
+num_nodes = 6
+num_edges = 7
 seed = 123
 G = None
 routesSet = None
 num_routes = 435
+
+def dijsktra(matrix, size, start):
+    minDist = [INTEGER_MAX] * size
+    minDist[start] = 0
+
+    minPath = [0] * size
+    used = [False] * size
+
+    for i in range(size):
+        nearest = -1
+        for v in range(size):
+            if ((not used[v]) and (nearest == -1 or minDist[nearest] > minDist[v])):
+                nearest = v
+        
+        if (minDist[nearest] == INTEGER_MAX):
+            continue
+        used[nearest] = True
+
+        for v in range(size):
+            len = minDist[nearest] + matrix[nearest][v]
+            if (minDist[v] > len):
+                if (matrix[nearest][v] != -1):
+                    minDist[v] = int(len)
+                    minPath[v] = int(nearest)
+
+    return minDist, minPath
+
 def createGraph():
     global G
     G = nx.gnm_random_graph(num_nodes, num_edges, seed)
+    
+    random.seed(seed)
+    for u, v in G.edges():
+        G[u][v]['weight'] = random.randint(100, 1000)
+    
     adj_matrix = nx.to_numpy_array(G)
     matrixLen = adj_matrix.shape
     for i in range(matrixLen[0]):
         for j in range(matrixLen[0]):
             if (adj_matrix[i][j] == 0):
-                adj_matrix[i][j] = 2147483647
+                adj_matrix[i][j] = -1
             if (i == j):
                 adj_matrix[i][j] = 0
-    
-    random.seed(seed)
-    for u, v in G.edges():
-        G[u][v]['weight'] = random.randint(100, 1000)
     
     global routesSet
     routesSet = set()
@@ -112,7 +142,13 @@ def createGraph():
     global num_routes
     num_routes = routesSet.__len__()
     
-    
+    print(adj_matrix)
+    for route in routesSet:
+        minDist, minPath = dijsktra(adj_matrix, num_nodes, route[0] - 1)
+        print(route[0] - 1)
+        print(minDist)
+        print(minPath)
+        break
 
 def insertStations(dbConnect):
     with dbConnect.cursor() as cursor:

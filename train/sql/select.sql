b384ceac3ae94f523d3e896eb6225b6630e27f2f
e.davydov <e.davydov@g.nsu.ru>
2025-02-22 13:57:55 +0700

    commit

--- train/sql/select.sql
+++ train/sql/select.sql
@@ -6,38 +6,6 @@ select count(*) from routes;
 select count(*) from booking;
 select count(*) from railroads_cars_booking;
 
-select rcb.*, s.* from railroads_cars_booking rcb
-    inner join schedule s on rcb.arrival_point = s.id
-        order by s.departure_time
-            limit 1;
-/* 01-01 || 07-20 */
-
-select count(*), DATE(s.arrival_time) as trip_date from railroads_cars_booking rcb
-    inner join schedule s on rcb.departure_point = s.id
-        inner join routes_structure rs on s.route_structure_id = rs.id
-            where rs.station_number_in_route = 0
-group by trip_date;
-
-select rcb.id, rcb.railroad_car_id, s.*, rs.station_number_in_route from railroads_cars_booking rcb
-inner join schedule s on (rcb.arrival_point = s.id or rcb.departure_point = s.id)
-inner join routes_structure rs on s.route_structure_id = rs.id
-inner join routes_structure rs on
-where rcb.id = 1
-order by rs.station_number_in_route;
-
-select rcb.id, rcb.railroad_car_id, s.*, rs2.id, rs2.route_id, rs2.station_number_in_route, rs2.distance
-from railroads_cars_booking rcb
-inner join schedule s on (rcb.arrival_point = s.id)
-inner join routes_structure rs on s.route_structure_id = rs.id
-inner join routes_structure rs2 on rs.route_id = rs2.route_id
-where rcb.id = 1
-order by rs2.station_number_in_route, s.id;
-
-select rcb.id, s.*, (select s2.thread_id from schedule s2 where s2.id = rcb.arrival_point)
-from railroads_cars_booking rcb
-inner join schedule s on rcb.departure_point = s.id
-where rcb.id = 1;
-
 update railroads_cars_booking rcb set arrival_point = 5 where rcb.id = 1;
 
 select count(distinct thread_id), count(distinct rcb.id), sum(distance),

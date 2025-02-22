fbb8b30fbc612ea64cb661d0cc1b85105dc2c128
e.davydov <e.davydov@g.nsu.ru>
2025-02-22 21:56:03 +0700

    initial

---
+++ abs/src/test/java/ru/improve/abs/AbsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.improve.abs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

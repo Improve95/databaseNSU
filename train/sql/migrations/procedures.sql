2edc25bf62b6c9cb68b8717d53cfa36640a66312
e.davydov <e.davydov@g.nsu.ru>
2025-02-25 23:53:09 +0700

    commit

--- train/sql/migrations/procedures.sql
+++ train/sql/migrations/procedures.sql
@@ -15,7 +15,16 @@ create type report as (
     calc_quarter timestamp,
     calc_year timestamp
 );
-
+drop table if exists report_by_day;
+create table report_by_day (
+    thread_count int,
+    passenger_count int,
+    distance_sum int,
+    calc_day timestamp,
+    calc_quarter timestamp,
+    calc_year timestamp
+);
+create extension if not exists hstore;
 drop function get_trip_report();
 create or replace function get_trip_report() returns report[] as $$
 declare
@@ -34,10 +43,10 @@ declare
     departure_rs routes_structure%rowtype;
     arrival_rs routes_structure%rowtype;
 
-    trip_data full_trip[];
-    full_trip_data full_trip[];
+    td full_trip;
 
     report_by_day report[];
+    report_by_day2 hstore := hstore('');
 begin
     for rcb_el in
         select rcb.* from railroads_cars_booking rcb
@@ -50,16 +59,25 @@ begin
         select rs.* into departure_rs from routes_structure rs where id = departure_point_route;
         select rs.* into arrival_rs from routes_structure rs where id = arrival_point_route;
 
-        select array(
+        foreach td in array(
             select row(rcb_el.id, s.thread_id, s.departure_time, rs.station_number_in_route, rs.distance) from routes_structure rs
                 inner join schedule s on rs.id = s.route_structure_id
             where rs.id between departure_rs.id and arrival_rs.id)
-        into trip_data;
+        loop
+            if (select count(*) from report_by_day rbd where rbd.calc_day > 0) then
 
-        full_trip_data := array_cat(full_trip_data, trip_data);
+            else
+                insert into report_by_day values (1, 1, );
+            end if;
+--             report_by_day2
+        end loop;
+
+
+
+--         full_trip_data := array_cat(full_trip_data, trip_data);
     end loop;
 
-    select array (
+   /*select array (
         select row(
             count(distinct (ftd::full_trip).thread_id),
             count(distinct (ftd::full_trip).id),
@@ -75,7 +93,7 @@ begin
             DATE((ftd::full_trip).departure_time)
             )
         having DATE_TRUNC('year', (ftd::full_trip).departure_time) is not null
-    ) into report_by_day;
+    ) into report_by_day;*/
 
     return report_by_day;
 end;
